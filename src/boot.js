a19dea5330eefff369f082af558cc0cb6f96141c
robmadeyou <robmadeyou@gmail.com>
2017-11-16 21:35:57 +0000

    Tidying up the repo, and saving to off site

---
+++ src/boot.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+var HatCommunicator = require('./bridges/HatCommunicator');
+
+setInterval(loop, 15);
+
+var hat = new HatCommunicator(2);
+hat.spawn();
+
+function loop() {
+  hat.write([0,0,0,0,0,255,255,255]);
+};
---
+++ src/bridges/HatCommunicator.js
@@ -0,0 +1,42 @@
+var spawn = require('child_process').spawn;
+var shell = require('shelljs');
+var fs = require('fs');
+
+const COMM_FILES = '/tmp/bandit/hat/';
+
+function HatCommunicator(pin) {
+  this.process;
+  this.dataString = '';
+  this.pin = pin;
+  this.commFile = COMM_FILES + Math.floor(Math.random() * 432);
+
+  var self = this;
+
+  function createProcess() {
+    if (!self.process) {
+      self.process = spawn('python', [__dirname + './python/HAT.py']);
+      self.process.stdout.on('end', function(){
+        self.process = null;
+      });
+    }
+  };
+
+  this.spawn = function() {
+    createProcess();
+
+    if (!fs.existsSync(COMM_FILES)) {
+      shell.mkdir('-p', COMM_FILES);
+    }
+
+    console.log('Spawned new process with file: ' + self.commFile);
+  };
+
+  this.write = function(obj) {
+    if (!self.process) createProcess();
+    fs.appendFile(this.commFile, JSON.stringify(obj));
+  }
+
+  return this;
+};
+
+module.exports = HatCommunicator;
