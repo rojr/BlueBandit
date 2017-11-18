b1b413be52b73031f20d4253c72ca3a577572079
robmadeyou <robmadeyou@gmail.com>
2017-11-18 02:27:11 +0000

    use join rather than string

--- src/bridges/HatCommunicator.js
+++ src/bridges/HatCommunicator.js
@@ -1,6 +1,7 @@
 var spawn = require('child_process').spawn;
 var shell = require('shelljs');
 var fs = require('fs');
+var path = require('path');
 
 const COMM_FILES = '/tmp/bandit/hat/';
 
@@ -16,7 +17,7 @@ function HatCommunicator(pin) {
     if (!self.process) {
       var procPath = __dirname + './python/HAT.py ' + self.commFile;
       console.log(procPath);
-      self.process = spawn('python', ['"' + __dirname + '/../python/HAT.py" ' + self.commFile]);
+      self.process = spawn('python', [path.join(__dirname, '..', 'python', 'HAT.py') + self.commFile]);
       self.process.on('exit', function(response) {
         console.log('Process died... ' + response);
         self.process = null;
