9c983fd265e31f9a265910682a5dc67e47ea425b
robmadeyou <robmadeyou@gmail.com>
2017-11-18 02:29:09 +0000

    ass a new array

--- src/bridges/HatCommunicator.js
+++ src/bridges/HatCommunicator.js
@@ -17,7 +17,7 @@ function HatCommunicator(pin) {
     if (!self.process) {
       var procPath = __dirname + './python/HAT.py ' + self.commFile;
       console.log(procPath);
-      self.process = spawn('python', [path.join(__dirname, '..', 'python', 'HAT.py') + self.commFile]);
+      self.process = spawn('python', [path.join(__dirname, '..', 'python', 'HAT.py'), self.commFile]);
       self.process.on('exit', function(response) {
         console.log('Process died... ' + response);
         self.process = null;
