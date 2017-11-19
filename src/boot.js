cf9bf4e09146b7c5d475a3b483ef576e9191f74b
robmadeyou <robmadeyou@gmail.com>
2017-11-19 21:15:31 +0000

    pure white

--- src/boot.js
+++ src/boot.js
@@ -14,7 +14,7 @@ function loop() {
        var jArray = [];
        for (var j = 0; j < 8; j++) {
          if (j == incc % 8) {
-           jArray.push([Math.floor(Math.random() * 200) + 55, Math.floor(Math.random() * 200) + 55, Math.floor(Math.random() * 200) + 55]);
+           jArray.push([255, 255, 255]);
          } else {
            jArray.push([0,0,0]);
          }
